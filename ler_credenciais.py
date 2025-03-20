d43d5e017de72cabd448a0e960901e7b1ee40480
Brenno <brennoprogmm@gmail.com>
2025-03-19 22:12:27 -0300

    Ajuste de leitura de credenciais. Inicio de desenvolvimento de login

---
+++ ler_credenciais.py
@@ -0,0 +1,7 @@
+import json
+
+with open("./credentials.json", "r") as credentialsFile:
+    credenciais =   json.load(credentialsFile)
+
+emailLinkedin = credenciais["EMAIL"]
+senhaLinkedin = credenciais["SENHA"]
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+from ler_credenciais import emailLinkedin
+from ler_credenciais import senhaLinkedin
+
 # Setup Selenium with automatic WebDriver management
 service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
@@ -14,11 +17,21 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://www.linkedin.com/")
 
 
-
-# A
-element = driver.find_element(By.XPATH, "/html/body/nav/div/a[normalize-space(text())='Entrar']")
-element.click()
+botaoEntrarLinkedin = driver.find_element(By.XPATH, "/html/body/nav/div/a[normalize-space(text())='Entrar']")
+botaoEntrarLinkedin.click()
 print("Button pressed")
+
+campoUserLogin =  driver.find_element(By.ID, "username")
+campoSenhaLogin =  driver.find_element(By.ID, "password")
+campoUserLogin.send_keys(emailLinkedin)
+campoSenhaLogin.send_keys(senhaLinkedin)
+
+
+#botaoEntrarLinkedin = driver.find_element(By.XPATH, "/html/body/nav/div/a[normalize-space(text())='Entrar']").click()
+
+#botao =  driver.find_element(By.ID, "password")
 
 import time
 time.sleep(2)
+
+input("Esperar")
