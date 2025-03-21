796dd00b5774a2ba53c7fee833315c40db94fc0b
Brenno <brennoprogmm@gmail.com>
2025-03-21 01:46:18 -0300

    Configuração de opções do Chromium para navegação stealth

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+import time
+import random
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-from ler_credenciais import emailLinkedin
-from ler_credenciais import senhaLinkedin
+from ler_credenciais import emailLinkedin, senhaLinkedin, userAgentNavegador
 
-# Setup Selenium with automatic WebDriver management
+
 service = Service(ChromeDriverManager().install())
+
+# Considerando que devemos tomar cuidado para que a conta não seja bloqueada por ser um bot, sera necessário simular comportamentos de um humano no crawler
+# Essa simulação começa com as configuração do Chrome que será utilizado pelo Selenium:
 options = webdriver.ChromeOptions()
 
-# Open browser
+# - Disable Blink:
+# Parece que o Chromium utiliza uma engine chamada "Blink" para rodar automações no navegador.
+# Essa engine anuncia para o JavaScript que está sendo utilizado ao abrir um site, então precisamos desativar isso para que não seja dectável. 
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+# Maximaze Window
+# - Isso é um pouco Overkill, mas pode ajudar. O código abaixo garante que o navegador vai ser aberto em tela cheia, já que o tamanho da tela é um dos indicadores de que não é humano acessando o site. 
+options.add_argument("--start-maximized")
+
+# - USER-AGENT:
+# É um argumento enviado para o site com algumas informações sobre o seu navegador. O selenium usar um user-agent genérico, então aplicar esse diferencial é uma boa vantagem
+options.add_argument(
+    f'--user-agent={userAgentNavegador}'
+)
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+
+
 driver = webdriver.Chrome(service=service, options=options)
+
+
+# Abaixo, listamos alguns objetos JavaScript o Selenuim deixa vazio ou preenchido de forma estranha e alteramos o valor deles para algo mais natural, que não será facilmente detectado
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+        Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
+        Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
+        Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
+    """
+})
 
 # Open Google
 driver.get("https://www.linkedin.com/")
@@ -31,8 +64,6 @@ campoSenhaLogin.send_keys(senhaLinkedin)
 
 #botao =  driver.find_element(By.ID, "password")
 
-
-import time
 time.sleep(2)
 
 input("Esperar")
