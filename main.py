d68f452d7335dcb3eee5833bd63bc6da0dbfd237
Brenno <brennoprogmm@gmail.com>
2025-03-28 00:07:55 -0300

    Primeiro login, sem cookies. Limpeza da main.py

--- main.py
+++ main.py
@@ -5,60 +5,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-
-
-service = Service(ChromeDriverManager().install())
-
-# Considerando que devemos tomar cuidado para que a conta não seja bloqueada por ser um bot, sera necessário simular comportamentos de um humano no crawler
-# Essa simulação começa com as configuração do Chrome que será utilizado pelo Selenium:
-options = webdriver.ChromeOptions()
-
-# - Disable Blink:
-# Parece que o Chromium utiliza uma engine chamada "Blink" para rodar automações no navegador.
-# Essa engine anuncia para o JavaScript que está sendo utilizado ao abrir um site, então precisamos desativar isso para que não seja dectável. 
-options.add_argument("--disable-blink-features=AutomationControlled")
-
-# Maximaze Window
-# - Isso é um pouco Overkill, mas pode ajudar. O código abaixo garante que o navegador vai ser aberto em tela cheia, já que o tamanho da tela é um dos indicadores de que não é humano acessando o site. 
-options.add_argument("--start-maximized")
-
-# - USER-AGENT:
-# É um argumento enviado para o site com algumas informações sobre o seu navegador. O selenium usar um user-agent genérico, então aplicar esse diferencial é uma boa vantagem
-options.add_argument(
-    f'--user-agent={USER_AGENT}'
-)
-
-# - Desativar enable-automation
-# A opção de "enable-automation" é umas das formas que o Selenium informa o site que é uma automaçã oque está acessando ele. 
-# Ele é responsével pelo banner de “Chrome is being controlled by automated test software” e por modificar alguns objetos que o JavaScript pode ler para saber se é uma automação acessando o site ou não
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-
-# Desativer extensão
-# O Selenium usa uma extensão junto com o Chromium para facilitar encontrar alguns elementos na página. Essa extensão deixa rastro e é uma das formas como sites vão verificar se é ou não um bot, então é necessário ser desativado
-options.add_experimental_option("useAutomationExtension", False)
-
-
-driver = webdriver.Chrome(service=service, options=options)
-
-
-# Abaixo, listamos alguns objetos JavaScript o Selenuim deixa vazio ou preenchido de forma estranha e alteramos o valor deles para algo mais natural, que não será facilmente detectado
-driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": """
-        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-        Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-        Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
-        Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
-    """
-})
-
-# Open Google
-driver.get("https://www.linkedin.com/")
-
-
-botaoEntrarLinkedin = driver.find_element(By.XPATH, "/html/body/nav/div/a[normalize-space(text())='Entrar']")
-botaoEntrarLinkedin.click()
-print("Button pressed")
 
 campoUserLogin =  driver.find_element(By.ID, "username")
 campoSenhaLogin =  driver.find_element(By.ID, "password")
--- primeiro_acesso.py
+++ primeiro_acesso.py
@@ -1,3 +1,16 @@
 # Rode esse script se esse for seu primeiro acesso ao linkedin utilizando essa automação.
 # Leia README.md
+import time
+from  selenium.webdriver.common.by import By
+from config_driver import configurar_driver
 
+driver = configurar_driver()
+
+driver.get("https://www.linkedin.com/")
+
+
+botaoEntrarLinkedin = driver.find_element(By.XPATH, "/html/body/nav/div/a[normalize-space(text())='Entrar']")
+botaoEntrarLinkedin.click()
+print("Preencha suas credenciais do Linkedin")
+
+time.sleep(30)
